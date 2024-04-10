8e3c17e353f51ebf0321d9fc1d71452e9133e1e4
Devi Sri Sai Charan <devisrisaicharan2@gmail.com>
2024-04-10 21:09:55 +0530

    added versel analytics package and component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import {Header} from './components/Header'
 import {Slider} from './components/Slider'
 import {Channels} from './components/Channels'
 import { MovieGeners}  from './components/MovieGeners'
+import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 
 function App() {
  
   return (
+    <Analytics>
     <div className='hi h-full'>
       <Header />
       <Slider />
       <Channels/>
       <MovieGeners />
     </div>
+    </Analytics>
   )
 }
 
