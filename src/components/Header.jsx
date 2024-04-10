4508fec7306cf0e985e25b08db9fb4e492d664eb
Devi Sri Sai Charan <devisrisaicharan2@gmail.com>
2024-04-10 21:03:43 +0530

    third commit changes done in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,8 +37,8 @@ export function Header(){
     return (
         <div className='h-18 py-2  px-4 bg-[rgb(0, 0, 39)] flex items-center'>
             <div className=' p-2 px-0 w-full flex justify-between md:justify-around gap-2 items-center'>
-                <img src={logo} className='h-10 md:h-12'/>
-                <div className=' flex  gap-6 font-semibold  text-white'>
+                <img src={logo} className='h-8 md:h-12'/>
+                <div className=' flex  gap-4 font-semibold  text-white'>
                     {menu.map((m) => <Nav Icon={m.icon} Title={m.title}/>)}
                 </div>
                 <div >
